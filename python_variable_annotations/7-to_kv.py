eefc28d98d0c5bc496b6f54552139779296d5fcc
Axel Malka <axelmalka@outlook.fr>
2023-08-28 10:16:49 +0000

    string and int/float to tuple

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,20 @@
+from typing import Union, Tuple
+"""
+takes a string k and an int OR float
+v as arguments and returns a tuple
+"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """
+    Creates a tuple with the string k and the square of v as a float.
+
+    Args:
+        k (str): The input string.
+        v (Union[int, float]): The input integer or float.
+
+    Returns:
+        Tuple[str, float]: A tuple containing k and the square of v as a float.
+    """
+    squared_value = float(v) ** 2
+    return k, squared_value
