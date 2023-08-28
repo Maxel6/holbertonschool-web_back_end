f249b57a21a073bc99602176a24b03b102611ed9
Axel Malka <axelmalka@outlook.fr>
2023-08-28 11:11:57 +0000

    len of elements

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Computes the sum of a list of floats."""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Takes an iterable of sequences and returns a list of tuples.
+    
+    Each tuple contains the original sequence and its length.
+
+    Args:
+        lst (Iterable[Sequence]): An iterable containing sequences.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples where each tuple contains
+        a sequence from lst and its length as an integer.
+    """
+    return [(i, len(i)) for i in lst]
