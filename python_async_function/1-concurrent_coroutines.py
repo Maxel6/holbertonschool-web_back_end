4ed7cef2fc1b8b70997c6cb3fffa3cce04b5e604
Axel Malka <axelmalka@outlook.fr>
2023-08-28 11:50:46 +0000

    corrected pycodestyle

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -21,7 +21,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     tasks = [wait_random(max_delay) for _ in range(n)]
     delays = await asyncio.gather(*tasks)
-    
+
     sorted_delays = sorted(delays)
-    
+
     return sorted_delays
