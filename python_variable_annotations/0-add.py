0bbe28d21f80bac0e2b9237fb605f9c0b68943a4
Axel Malka <axelmalka@outlook.fr>
2023-08-28 09:41:00 +0000

    typecast variables

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+    Adds two floats and returns their sum.
+
+    Args:
+        a (float): The first float.
+        b (float): The second float.
+
+    Returns:
+        float: The sum of a and b as a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    return a + b
