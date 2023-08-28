58c9c5dae558e53c3daf8348467c8d0a0da17bd2
Axel Malka <axelmalka@outlook.fr>
2023-08-28 11:46:43 +0000

    adding wait_n function

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -2,7 +2,7 @@
 """asynchronous coroutine that takes in an integer argument"""
 import asyncio
 from typing import List
-from python_async_function import wait_random
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
