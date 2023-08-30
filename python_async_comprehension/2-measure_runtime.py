1ca98280f0294c2cc53d71be7fdb5c5cbdf90851
Axel Malka <axelmalka@outlook.fr>
2023-08-30 08:54:57 +0000

    fim import issues

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -2,7 +2,7 @@
 """measure_runtime should measure the total runtime and return it."""
 import asyncio
 import time
-async_comprehension = __import__("1-async_comprehension").async_comprehension
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
