896ddad6b339f984cc013c658b9fe00503eae7cb
Axel Malka <axelmalka@outlook.fr>
2023-08-30 09:11:20 +0000

    fix tracemalloc issues

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -3,24 +3,32 @@
 import time
 import asyncio
 from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     """
-    Mesure le temps d'exécution moyen de wait_n(n, max_delay).
+    Measures the average execution time for the `wait_n` function.
 
     Args:
-        n (int): Le nombre de fois à appeler wait_n.
-        max_delay (int): Le délai maximum en secondes pour
-        chaque appel à wait_n.
+        n (int): The number of times to call `wait_n`.
+        max_delay (int): The maximum delay in seconds for each `wait_n` call.
 
     Returns:
-        float: Le temps d'exécution moyen par appel en secondes.
+        float: The average execution time per call in seconds.
     """
-    start_time = time.time()
-    await asyncio.gather(*(wait_n(max_delay) for _ in range(n)))
-    end_time = time.time()
 
-    total_time = end_time - start_time
-    return total_time / n
+    async def async_measure():
+        """
+        An asynchronous function that measures the execution time of `wait_n`.
+
+        Returns:
+            float: The execution time in seconds for a single `wait_n` call.
+        """
+        start = time.time()
+        await wait_n(n, max_delay)
+        end = time.time()
+        return (end - start) / n
+
+    return asyncio.run(async_measure())
