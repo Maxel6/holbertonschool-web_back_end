9e603c8c42d0fe8d989b8eb7d5681ce6eb050e4e
Axel Malka <axelmalka@outlook.fr>
2023-09-15 10:42:20 +0000

    fix: add evacuationWarningMessage check

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,16 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (typeof sqft !== 'number') {
-      throw new Error('Square footage should be a number');
+    if (this.constructor !== Building) {
+      if (!this.evacuationWarningMessage) {
+        throw Error('Class extending Building must override evacuationWarningMessage');
+      }
     }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  static evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
